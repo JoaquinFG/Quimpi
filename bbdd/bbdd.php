da7ba7731fe54df3c6463be78d69d8ba8c96e373
JoaquinFG <joaquin.ferreira@babelgroup.com>
2023-02-24 13:34:07 +0100

    quimpi V2.0.0

--- bbdd/bbdd.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "conf_db.php";
-
-$conexion = mysqli_connect( $servidor, $usuario, "" );
-
-$db = mysqli_select_db( $conexion, $basededatos );
-?>
--- bbdd/conf_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$usuario = "root";
-$contrasena = "";  // en mi caso tengo contraseña pero en casa caso introducidla aquí.
-$servidor = "localhost";
-$basededatos = "q";
-?>
